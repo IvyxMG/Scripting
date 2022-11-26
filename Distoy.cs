f8e1762b43a1b13a19ff42c559e7bd69ca4902fd
IvyxMG <102462950+IvyxMG@users.noreply.github.com>
2022-11-26 17:14:49 +0300

    Add files via upload

--- Distoy.cs
+++ Distoy.cs
@@ -2,17 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class crickets : MonoBehaviour
+public class Distoy : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+        Destroy(gameObject, Cube);
     }
 
     // Update is called once per frame
     void Update()
     {
-        print("crickets");
+        
     }
 }
